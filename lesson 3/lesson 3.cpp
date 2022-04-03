3c2818ad1aa9122849c8ccbbf0198f40d9e9e099
Nazar Shpak <nazar.shpak2004@gmail.com>
2022-04-03 22:14:34 +0300

    Add project files.

---
+++ lesson 3/lesson 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    std::cout << " Enter the number: ";
+    std::cin >> a ;
+
+    std::cout << " Enter more number: ";
+    std::cin >> b;
+
+    std::cout << "Change the values in places \n";
+
+    if (b > a)
+    {
+        c = b;
+        b = a;
+        a = c;
+    }
+        std::cout << "Smaller number:" << b << std::endl;
+        std::cout << "More number:" << a << std::endl;
+  
+    return 0;
+}
